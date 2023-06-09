832372032b9dd2a67137be567c6ececc211a5c5a
Rachel Cheman <rachelcheman@gmail.com>
2023-06-09 18:10:52 -0400

    Adds telemetry data for logging and tracing. (Chapter 4)

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,20 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // web::Data lets us make a copy of the pool. When we run the server,
     // it will create servers for as many threads as are available.
     // So, its making copies of the server and we need to be able
     // to pass in a reference to a database pool so there can be several unique connections.
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
